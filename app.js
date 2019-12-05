36ad0013dd6fda8b3de30651198f1275fafecc21
Shenbaga Rajan <v.shenbaga.rajan@accenture.com>
2019-12-05 20:27:59 +0530

    pug

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const path = require('path')
+const path = require('path');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRouter = require('./Routes/admin');
-const shopRouter = require('./Routes/shop');
-app.use(bodyParser.urlencoded({ extended: 'false' }));
+app.set('view engine','pug')
+app.set('views','views')
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, './', 'Views', '404.html'))
-})
+    res.status(404).render('404',{pageTitle:'Page Not Found'})
+});
 
 app.listen(3000);
